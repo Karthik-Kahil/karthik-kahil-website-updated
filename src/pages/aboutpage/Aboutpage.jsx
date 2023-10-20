a085a9f3a5bbb79261360c4326acfe104459c6d8
Karthik-Kahil <skilled_results_0u@icloud.com>
2023-10-20 22:07:06 +0530

    components update

--- src/pages/aboutpage/Aboutpage.jsx
+++ src/pages/aboutpage/Aboutpage.jsx
@@ -1,8 +1,40 @@
 import styled from "styled-components";
 import HomeHeader from "../homepage/HomeHeader";
 
-const gridTwo = styled.div`
+const GridTwo = styled.div`
   display: grid;
+  grid-template-columns: 300px 1fr;
+  max-width: 1200px;
+  margin: 0 auto;
+  grid-gap: 8rem;
+
+  & div img {
+    width: 100%;
+    height: 100%;
+    max-width: 500px;
+    max-height: 400px;
+    border-radius: 50rem 50rem 0px 0px;
+  }
+
+  & div > div {
+    background-color: var(--color-green-a1);
+    color: var(--color-grey-0);
+    padding: 1.5rem;
+    margin-top: -10px;
+  }
+
+  & .right-grid > h2 {
+    font-family: "AcornRegular", sans-serif;
+    font-size: 5rem;
+    line-height: 6rem;
+  }
+
+  & p {
+    font-family: "Poppins", sans-serif;
+    font-size: 2rem;
+    line-height: 3rem;
+    margin-top: 2rem;
+  }
 `;
 
 function Aboutpage() {
@@ -16,14 +48,14 @@ function Aboutpage() {
           padding={"10rem"}
         />
       </div>
-      <div>
+      <GridTwo>
         <div>
           <img src="../../public/karthik-kahil-faces.jpg" alt="" />
           <div>
             <h2>Karthik Kahil</h2>
           </div>
         </div>
-        <div>
+        <div className="right-grid">
           <h2>
             I'm a Product Designer working remotely from 17°C Dublin, Ireland.
           </h2>
@@ -33,7 +65,7 @@ function Aboutpage() {
             UI/UX. I'm proud to have worn many hats.
           </p>
         </div>
-      </div>
+      </GridTwo>
     </>
   );
 }
