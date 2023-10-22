876125cce2f52ebdaa3b9b24ab7a4eaaa6ac9f20
Karthik-Kahil <skilled_results_0u@icloud.com>
2023-10-22 14:50:05 +0530

    components update

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-  padding: 3rem;
+  padding: 3rem 3rem;
   background-color: var(--color-secondary-a1);
   font-size: 10rem;
   text-align: center;
